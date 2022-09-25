ad6f657f31d6aac14662448f24f60976acec5b50
hamaa-affix <hamaagive1108@gmail.com>
2022-09-25 10:44:49 +0900

    userテーブルにtaxIdを追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('tax_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('tax_id')->references('id')->on('tax_rates');
         });
     }
 
